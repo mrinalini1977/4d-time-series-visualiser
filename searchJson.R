c0bf9f2e62b098d5b20b2adf98731474851d73fc
mrinalini1977 <mrinalinik@yahoo.com>
2018-04-02 12:43:18 +0530

    Added a meta file

--- searchJson.R
+++ searchJson.R
@@ -5,59 +5,225 @@ library(RJSONIO)
 library(dplyr)
 library(tidyr)
 library(tidyjson)
+library(gsubfn)
+library(lazyeval)
 
-buildData <- paste(readLines("data/buildResults.json"), collapse = " ")
+metaDef = read_json ("C:/work/4d-time-series-visualiser/metaDef.json")
 
-init <- buildData %>% 
-  enter_object("aggregations") %>%
-  enter_object("data")%>%
-  enter_object("buckets") %>%
-  gather_array(column.name = "agg_bucket_index")%>% 
-  enter_object("output")  %>%
-  enter_object("buckets") %>%
-  gather_array("output_bucket_index") %>%
-  enter_object("runs") %>%
-  enter_object("buckets") %>%
-  gather_array(column.name = "runs_bucket_index") %>% 
-  enter_object("latest") %>%
-  enter_object("hits")  %>%
-  enter_object("hits")  %>%
-  gather_array(column.name = "hits_bucket_index") %>%
-  enter_object("_source")
+metaDef_file <-metaDef %>% enter_object("file") %>% 
+  spread_values(wrk_dir = jstring("wrk_dir"))
 
-sourceobj <- init %>% 
+setwd(metaDef_file$wrk_dir)
+
+buildData = read_json("data/buildResults.json")
+
+axis_param <- c("time_axis", "z_axis", "y_axis", "x_axis", "bubble_axis")
+
+metaData_path <- metaDef %>% enter_object("axis_paths") %>%
   spread_values(
-    buildName = jstring("BuildName"),
-    time = jstring("timestamp"),
-    commits = jstring("Commits" ) ) %>%
-    tbl_df()
+    time_axis = jstring("time_axis"),
+    z_axis = jstring("z_axis"),
+    y_axis = jstring("y_axis"),
+    x_axis = jstring("x_axis"),
+    bubble_axis = jstring("bubble_axis"))
 
-artefactobj <- init %>% 
-    enter_object("artefacts") %>%
-  gather_array(column.name = "artefacts_index") %>%
-  spread_values( artefact_size = jnumber("size"),  
-                 artefact_name = jstring("name")) %>%
-  json_lengths() %>%
-  tbl_df()
+metaData_agg <- metaDef %>% enter_object("aggs")  
 
-artefactGroupobj<- artefactobj %>% group_by(agg_bucket_index, output_bucket_index, runs_bucket_index, hits_bucket_index) %>%
-  summarise(artefact_size_total = sum(artefact_size), artefact_count = n()) %>%
-  tbl_df()
+metaData_agg_keys<- metaData_agg %>% gather_keys()
 
-joinartefact <- inner_join(sourceobj, artefactGroupobj) %>%
-  select(-document.id,-agg_bucket_index, -output_bucket_index, -runs_bucket_index, -hits_bucket_index)
+time_axis_data <- getAxisData(buildData, metaData_path$time_axis, axis_param[1], metaData_agg)
+z_axis_data <- getAxisData(buildData, metaData_path$z_axis, axis_param[2],metaData_agg)
+y_axis_data <- getAxisData(buildData,metaData_path$y_axis, axis_param[3],metaData_agg)
+x_axis_data <- getAxisData(buildData,metaData_path$x_axis, axis_param[4],metaData_agg)
+bubble_axis_data <- getAxisData(buildData,metaData_path$bubble_axis, axis_param[5],metaData_agg)
 
 
-class(joinartefact)
+join_axis_data <- inner_join(time_axis_data,z_axis_data) %>% 
+  inner_join (.,y_axis_data) %>%
+  inner_join(.,x_axis_data) %>%
+  inner_join(.,bubble_axis_data) %>%
+  tbl_df
+
+df_build_data <- select(join_axis_data, time_axis_value, z_axis_value, y_axis_value, x_axis_value, bubble_axis_value)
+
+write.csv(as.matrix(df_build_data),"data/axisData.csv",row.names = F)
 
 
-df_build_data <- as.data.frame(joinartefact)
+###function to get the last character of a string ###
 
-df_build_data$buildDate <- as.Date(df_build_data$time)
-df_build_data$buildTime <- format(as.POSIXct(df_build_data$time, format = "%Y-%m-%dT%H:%M:%S", tz="GMT") ,format = "%H:%M:%S") 
-
-df_build_data = rename(df_build_data, dateTime = time)
-
-write.csv(as.matrix(within(df_build_data, rm("time"))),"data/buildExtract.csv")
+getLastChar <- function(data)
+{
+  return (substr(data, nchar(data),nchar(data)))
+}
 
 
+### function to get the aggregate parameters specified in the meta file ###
+getAggParams = function(m_agg, p_axis)
+{
+  ###grouping field
+  
+  axis_obj <- m_agg %>% enter_object(p_axis) 
+  
+  paramNames <- c("groupByParam", "funParam", "fieldParam")
+  
+  groupBy_name <- axis_obj %>% enter_object("terms") %>% 
+    spread_values( field = jstring("field") )
+  
+  ### function field and type
+  fun_obj <- axis_obj %>% enter_object("aggs") %>% gather_keys
+  
+  fun_name <- axis_obj %>% enter_object("aggs") %>% 
+    enter_object(fun_obj$key) %>% 
+    spread_values( field_n = jstring("field"))
+  
+  
+  paramValues <- c(groupBy_name$field, fun_obj$key, fun_name$field_n)
+  
+  aggParams <-  setNames(as.list(paramValues), paramNames)
+  
+  return(aggParams)
+  
+}
+
+### function to aggregare the data based on the aggregate parameters ###
+getAxisData_agg = function(query, groupBy_vars, aggParams, p_axis)
+{
+  groupBy_vars_dots = sapply(groupBy_vars, . %>% 
+  {as.formula(paste0('~', .))})
+  funField <- aggParams$fieldParam
+  funName <- aggParams$funParam
+  
+  if (funName == "sum")
+  {
+    query_agg <- query %>% group_by_(.dots = groupBy_vars_dots)   %>%
+      summarise_(.dots = fn$list(val = "sum(as.numeric($funField))"))
+  }
+  
+  if(funName == "count")
+  {
+    query_agg <- query %>% group_by_(.dots = groupBy_vars_dots)   %>%
+      summarise(val = n()) 
+  }
+  
+  if(funName == "count_distinct")
+  {
+    query_agg <- query %>% group_by_(.dots = groupBy_vars_dots)   %>%
+      summarise(val = n_distinct()) 
+  }
+  
+  if (funName == "mean")
+  {
+    query_agg <- query %>% group_by_(.dots = groupBy_vars_dots)   %>%
+      summarise_(.dots = fn$list(val = "mean(as.numeric($funField))"))
+  }
+  
+  if (funName == "min")
+  {
+    query_agg <- query %>% group_by_(.dots = groupBy_vars_dots)   %>%
+      summarise_(.dots = fn$list(val = "min(as.numeric($funField))"))
+  }
+  
+  if (funName == "max")
+  {
+    query_agg <- query %>% group_by_(.dots = groupBy_vars_dots)   %>%
+      summarise_(.dots = fn$list(val = "max(as.numeric($funField))"))
+  }
+  
+  if (funName == "quantile")
+  {
+    query_agg <- query %>% group_by_(.dots = groupBy_vars_dots)   %>%
+      summarise_(.dots = fn$list(val = "quantile(as.numeric($funField))"))
+  }
+  
+  
+  names(query_agg)[names(query_agg) == "val"] <- paste0(p_axis,"_value")
+  
+  return(query_agg)
+}
+
+### main function that extracts the value from the json file ###
+getAxisData = function(buildData, m_data, p_axis,m_agg)
+{
+  query = NULL
+  
+  axis_v <- unlist(strsplit(m_data, "->"))
+  
+  axis_length <- length(axis_v)
+  
+  m_agg_keys <- m_agg %>% gather_keys()
+  
+  aggRequired <- FALSE
+  group_by_values_complete <- FALSE
+  groupBy_vars <- c()
+  
+  if (is.element(p_axis, m_agg_keys$key))
+  {
+    
+    aggRequired <- TRUE
+    
+    aggParams <- getAggParams(m_agg, p_axis)
+    
+    groupBy_name <-  aggParams$groupByParam
+    fun_name <- aggParams$funParam
+    fun_field <- aggParams$fieldParam
+  }
+  
+  for (i in 1:axis_length)
+  {
+    element = trimws(axis_v[i])
+    
+    if( i == 1)
+    {
+      query <- buildData
+    }
+    
+    lastChar <- getLastChar(element)
+    
+    
+    if (i==axis_length)
+    {
+      query <- query %>% spread_values( val = jstring(element))
+      
+      if(aggRequired)
+      {
+        names(query)[names(query) == "val"] <- fun_field 
+      }
+      else
+      {
+        names(query)[names(query) == "val"] <- paste0(p_axis,"_value")
+      }
+    }
+    
+    else 
+    {
+      if( lastChar != '*')
+      {
+        query <- query %>% enter_object(element)
+      }
+      else 
+      {
+        element <- substr(element,1,nchar(element)-1)
+        index <- paste0(element, "_", i,"_index")
+        query <-  query %>% enter_object(element) %>% gather_array(index)
+        
+        if(aggRequired && is.element(element, groupBy_name))
+        {
+          group_by_values_complete = TRUE
+        }
+        
+        if(aggRequired && !group_by_values_complete)
+        {
+          groupBy_vars <- c(groupBy_vars, index)
+        }
+      }
+    }
+  }
+  
+  if(aggRequired)
+  {
+    
+    query <- getAxisData_agg(query, groupBy_vars, aggParams, p_axis)
+  }
+  
+  return (query)
+}
